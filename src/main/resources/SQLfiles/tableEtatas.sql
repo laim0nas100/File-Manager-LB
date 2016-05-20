25c28c02f2c1fa1f21866ff52e2d26ce5bbe93b2
Lemmin <Lemmin@Acer-WINDOWS>
2016-05-20 05:35:06 +0300

    Snapshot task update

--- src/main/resources/SQLfiles/tableEtatas.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE labe2219.ETATAS (
-Pareigos VARCHAR(50) not NULL,
-IlgisValandomis FLOAT,
-Alga FLOAT,
-PRIMARY KEY (Pareigos) )
--- src/main/resources/SQLfiles/tablePreke.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE labe2219.PREKE (
-PrekesID VARCHAR(15) not NULL,
-Kaina double precision not NULL CHECK (Kaina >0),
-Aprasas VARCHAR(1000),
-Yra boolean DEFAULT TRUE,
-PRIMARY KEY (PrekesID) 
-)
